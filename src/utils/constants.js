2f0e8110d2e5314a064b09f34caab925e92a4702
Wenodh <vinodkumara3@gmail.com>
2024-01-29 22:56:00 +0530

    updated url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,10 @@
 export const RESTAURANT_CDN_URL =
     'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/';
 export const MENU_API =
-    'https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.5248148&lng=78.4497875&restaurantId=';
+    'https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.5248148&lng=78.4497875&restaurantId=';
 
 export const RESTAURANTS_URL =
-    'https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.5248148&lng=78.4497875&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
+    'https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.5248148&lng=78.4497875&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
 
 export const CATEGORY_CDN_URL =
     "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/";
